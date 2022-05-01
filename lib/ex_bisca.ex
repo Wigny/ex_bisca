ffb7626e68a9904da64b5288f5afa07fa8cc2903
Wígny Almeida <wignybora@gmail.com>
2022-05-01 17:29:37 +0000

    feat: initial commit

---
+++ lib/ex_bisca.ex
@@ -0,0 +1,18 @@
+defmodule ExBisca do
+  @moduledoc """
+  Documentation for `ExBisca`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBisca.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
