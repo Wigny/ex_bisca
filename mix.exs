dda85afa2bb6e56f188d512e784e8d238df23efb
Wígny Almeida <wignybora@gmail.com>
2022-05-02 18:23:56 +0000

    chore: add petal components

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule ExBisca.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:petal_components, "~> 0.15"},
       {:typed_struct, "~> 0.3"},
       {:mix_test_watch, "~> 1.1", only: :dev, runtime: false}
     ]
