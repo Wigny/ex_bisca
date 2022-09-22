642b1f20adab4454e390468ad36715261ab9b66c
Wígny Almeida <wignybora@gmail.com>
2022-09-22 13:49:52 -0400

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule ExBisca.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.9"},
+      {:phoenix, "~> 1.6.12"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
